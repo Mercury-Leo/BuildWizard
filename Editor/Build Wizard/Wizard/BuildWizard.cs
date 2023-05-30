819db5c704943debe47078ad21f96b7cc84a61f4
Mercury <kansamas@gmail.com>
2023-05-30 12:07:17 +0300

    Update BuildWizard.cs
    - Fixed Branch name
    - Added commit version to preview

--- Editor/Build Wizard/Wizard/BuildWizard.cs
+++ Editor/Build Wizard/Wizard/BuildWizard.cs
@@ -19,281 +19,286 @@ using UnityEngine;
 
 namespace Editor.Build_Wizard.Wizard
 {
-    public class BuildWizard : OdinEditorWindow
+   public class BuildWizard : OdinEditorWindow
     {
         private static readonly GUIContent WindowTitle = new("Build Wizard");
 
-    [BoxGroup("Build Type")] [InfoBox("Builds a clean release exe")] [SerializeField]
-    private bool releaseBuild;
+        [BoxGroup("Build Type")] [InfoBox("Builds a clean release exe")] [SerializeField]
+        private bool releaseBuild;
 
-    [BoxGroup("Build Type")] [InfoBox("Builds a development exe")]
-    public bool debugBuild;
+        [BoxGroup("Build Type")] [InfoBox("Builds a development exe")]
+        public bool debugBuild;
 
-    [BoxGroup("Build Type")] [InfoBox("Builds a development exe with Deep profiling enabled")] [SerializeField]
-    private bool deepProfileBuild;
+        [BoxGroup("Build Type")] [InfoBox("Builds a development exe with Deep profiling enabled")] [SerializeField]
+        private bool deepProfileBuild;
 
-    [FolderPath(RequireExistingPath = true, AbsolutePath = true)] [SerializeField]
-    private string buildLocation = string.Empty;
+        [FolderPath(RequireExistingPath = true, AbsolutePath = true)] [SerializeField]
+        private string buildLocation = string.Empty;
 
-    [FoldoutGroup("Version", order: 3), ReadOnly] [ShowInInspector]
-    private int _major;
+        [FoldoutGroup("Version", order: 3), ReadOnly] [ShowInInspector]
+        private int _major;
 
-    [FoldoutGroup("Version"), ReadOnly] [ShowInInspector]
-    private int _minor;
+        [FoldoutGroup("Version"), ReadOnly] [ShowInInspector]
+        private int _minor;
+        
+        [FoldoutGroup("Version"), ReadOnly] [ShowInInspector]
+        private int Commit => int.Parse(GitExtensions.GetNumberOfCommits());
 
-    private const BuildOptions ReleaseOptions = BuildOptions.CleanBuildCache | BuildOptions.ShowBuiltPlayer;
+        private const BuildOptions ReleaseOptions = BuildOptions.CleanBuildCache | BuildOptions.ShowBuiltPlayer;
 
-    private const BuildOptions DebugOptions =
-        BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ShowBuiltPlayer |
-        BuildOptions.ConnectToHost;
+        private const BuildOptions DebugOptions =
+            BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ShowBuiltPlayer |
+            BuildOptions.ConnectToHost;
 
-    private const BuildOptions DeepProfileOptions =
-        DebugOptions | BuildOptions.EnableDeepProfilingSupport | BuildOptions.DetailedBuildReport;
+        private const BuildOptions DeepProfileOptions =
+            DebugOptions | BuildOptions.EnableDeepProfilingSupport | BuildOptions.DetailedBuildReport;
 
-    private const string ReleaseFolder = "Release";
-    private const string DebugFolder = "Debug";
-    private const string DeepProfileFolder = "DeepProfile";
-    private const string BuildFolder = "Builds";
-    private const string AbsorberName = "Launcher";
-    private const string FileName = AbsorberName + ".exe";
-    private const string VersionInformationName = "VersionInformation.txt";
-    private const string EditorBuildLocationKey = "LastBuildLocation";
-    private const string ResourcesFolder = "Resources";
-    private const string ProjectVersion = "Project Version";
-    private const string Assets = "Assets";
-    private const string AssetFileEnding = ".asset";
-    private const string Pattern = @"^(.*\/){0,1}(.{1,10})";
-    private const string Dash = "-";
+        private const string ReleaseFolder = "Release";
+        private const string DebugFolder = "Debug";
+        private const string DeepProfileFolder = "DeepProfile";
+        private const string BuildFolder = "Builds";
+        private const string AbsorberName = "Launcher";
+        private const string FileName = AbsorberName + ".exe";
+        private const string VersionInformationName = "VersionInformation.txt";
+        private const string EditorBuildLocationKey = "LastBuildLocation";
+        private const string ResourcesFolder = "Resources";
+        private const string ProjectVersion = "Project Version";
+        private const string Assets = "Assets";
+        private const string AssetFileEnding = ".asset";
+        private const string Pattern = @"^(.*\/){0,1}(.{1,10})";
+        private const string Dash = "-";
 
 #pragma warning disable CS0414
-    private bool _upgradedMajor;
-    private bool _upgradedMinor;
+        private bool _upgradedMajor;
+        private bool _upgradedMinor;
 #pragma warning restore CS0414
 
-    public static bool IsBuilding;
+        public static bool IsBuilding;
 
-    private readonly TextFileWriter _textFile = new();
+        private readonly TextFileWriter _textFile = new();
 
-    private ProjectVersionSO _projectVersion;
+        private ProjectVersionSO _projectVersion;
 
-    private void Awake()
-    {
-        _projectVersion = FindOrCreateProjectVersion();
-        SetVersionVisuals();
-
-        if (EditorPrefs.HasKey(EditorBuildLocationKey))
+        private void Awake()
         {
-            buildLocation = EditorPrefs.GetString(EditorBuildLocationKey);
-        }
-    }
-
-    [Button]
-    [DisableIf("@!(this.releaseBuild || this.debugBuild || this.deepProfileBuild)")]
-    public void Build()
-    {
-        if (!(releaseBuild || debugBuild || deepProfileBuild))
-        {
-            return;
+            _projectVersion = FindOrCreateProjectVersion();
+            SetVersionVisuals();
+            
+            if (EditorPrefs.HasKey(EditorBuildLocationKey))
+            {
+                buildLocation = EditorPrefs.GetString(EditorBuildLocationKey);
+            }
         }
 
-        CheckBuildLocation();
-
-        var version = SetVersion(true);
-        var scenes = GetBuildScenes();
-        var branchName = GetFolderBranchName(GitExtensions.Branch);
-
-        IsBuilding = true;
-        if (releaseBuild)
+        [Button]
+        [DisableIf("@!(this.releaseBuild || this.debugBuild || this.deepProfileBuild)")]
+        public void Build()
         {
-            BuildFlow(version, scenes, branchName, ReleaseFolder, ReleaseOptions);
-        }
-
-        version = SetVersion();
-
-        if (debugBuild)
-        {
-            BuildFlow(version, scenes, branchName, DebugFolder, DebugOptions);
-        }
-
-        if (deepProfileBuild)
-        {
-            BuildFlow(version, scenes, branchName, DeepProfileFolder, DeepProfileOptions);
-        }
-
-        IsBuilding = false;
-        Close();
-    }
-
-    [Button]
-    public void BuildAll()
-    {
-        releaseBuild = true;
-        debugBuild = true;
-        deepProfileBuild = true;
-        Build();
-    }
-
-    [HorizontalGroup("Version/Upgrade"), Button, DisableIf("_upgradedMajor")]
-    public void UpgradeMajor()
-    {
-        _projectVersion.UpgradeMajor();
-        _upgradedMajor = true;
-        SetVersionVisuals();
-    }
-
-    [HorizontalGroup("Version/Upgrade"), Button, DisableIf("_upgradedMinor")]
-    public void UpgradeMinor()
-    {
-        _projectVersion.UpgradeMinor();
-        _upgradedMinor = true;
-        SetVersionVisuals();
-    }
-
-    [MenuItem("Build/Wizard")]
-    private static void ShowBuildWindow()
-    {
-        var window = GetWindow<BuildWizard>();
-        window.titleContent = WindowTitle;
-        window.Show();
-    }
-
-    private void BuildFlow(string version, string[] scenes, string branchName, string folder, BuildOptions options)
-    {
-        var path = GetBuildPath(version, branchName, folder);
-        var buildOptions = GetBuildOptions(options, path, scenes);
-        BuildPlayer(buildOptions);
-        WriteBuildInformation(path, GitExtensions.FullCommitHash, GitExtensions.Branch, _projectVersion.FullVersion);
-    }
-
-    private void CheckBuildLocation()
-    {
-        if (string.IsNullOrWhiteSpace(buildLocation))
-        {
-            var parentFolder = Path.GetDirectoryName(Application.dataPath);
-            if (string.IsNullOrWhiteSpace(parentFolder))
+            if (!(releaseBuild || debugBuild || deepProfileBuild))
             {
                 return;
             }
 
-            buildLocation = Path.Combine(parentFolder, BuildFolder);
-        }
-    }
+            CheckBuildLocation();
 
-    private string[] GetBuildScenes()
-    {
-        var buildScenes = EditorBuildSettings.scenes;
-        var scenes = new string[buildScenes.Length];
-        for (int index = 0; index < buildScenes.Length; index++)
-        {
-            scenes[index] = EditorBuildSettings.scenes[index].path;
-        }
+            var version = SetVersion(true);
+            var scenes = GetBuildScenes();
+            var branchName = GetFolderBranchName(GitExtensions.Branch);
 
-        return scenes;
-    }
+            IsBuilding = true;
+            if (releaseBuild)
+            {
+                BuildFlow(version, scenes, branchName, ReleaseFolder, ReleaseOptions);
+            }
 
-    private string SetVersion(bool isRelease = false)
-    {
-        var version = isRelease ? _projectVersion.Version : _projectVersion.FullVersion;
+            version = SetVersion();
 
-        PlayerSettings.bundleVersion = version;
+            if (debugBuild)
+            {
+                BuildFlow(version, scenes, branchName, DebugFolder, DebugOptions);
+            }
 
-        return version;
-    }
+            if (deepProfileBuild)
+            {
+                BuildFlow(version, scenes, branchName, DeepProfileFolder, DeepProfileOptions);
+            }
 
-    private string GetBuildPath(string version, string branch, string folder)
-    {
-        var mainPath = buildLocation;
-
-        EditorPrefs.SetString(EditorBuildLocationKey, buildLocation);
-
-        var buildFolderPath = Path.Combine(mainPath, folder, branch, Dash, version);
-
-        if (!Directory.Exists(buildFolderPath))
-        {
-            Directory.CreateDirectory(buildFolderPath);
+            IsBuilding = false;
+            Close();
         }
 
-        return Path.Combine(buildFolderPath, FileName);
-    }
-
-    private BuildPlayerOptions GetBuildOptions(BuildOptions option, string path, string[] scenes)
-    {
-        return new BuildPlayerOptions
+        [Button]
+        public void BuildAll()
         {
-            options = option, locationPathName = path, target = BuildTarget.StandaloneWindows64, scenes = scenes
-        };
-    }
-
-    private void BuildPlayer(BuildPlayerOptions options)
-    {
-        var report = BuildPipeline.BuildPlayer(options);
-        var summary = report.summary;
-
-        if (summary.result is BuildResult.Succeeded)
-        {
-            Debug.Log("Build was a success");
-            return;
+            releaseBuild = true;
+            debugBuild = true;
+            deepProfileBuild = true;
+            Build();
         }
 
-        Debug.LogError("Failed build process!");
-    }
-
-    private void WriteBuildInformation(string path, params string[] data)
-    {
-        if (data == null)
+        [HorizontalGroup("Version/Upgrade"), Button, DisableIf("_upgradedMajor")]
+        public void UpgradeMajor()
         {
-            return;
+            _projectVersion.UpgradeMajor();
+            _upgradedMajor = true;
+            SetVersionVisuals();
         }
 
-        var contents = string.Empty;
-        foreach (var content in data)
+        [HorizontalGroup("Version/Upgrade"), Button, DisableIf("_upgradedMinor")]
+        public void UpgradeMinor()
         {
-            contents += content + "\n";
+            _projectVersion.UpgradeMinor();
+            _upgradedMinor = true;
+            SetVersionVisuals();
         }
 
-        var filePath = Path.Combine(Path.GetDirectoryName(path) ?? string.Empty, VersionInformationName);
-        _textFile.WriteToTextFileAt(filePath, contents);
-    }
-
-    private void SetVersionVisuals()
-    {
-        _major = _projectVersion.Major;
-        _minor = _projectVersion.Minor;
-    }
-
-    private ProjectVersionSO FindOrCreateProjectVersion()
-    {
-        var path = Path.Combine(Application.dataPath, ResourcesFolder);
-
-        if (!Directory.Exists(path))
+        [MenuItem("Build/Wizard")]
+        private static void ShowBuildWindow()
         {
-            Directory.CreateDirectory(path);
-            CreateProjectVersion();
+            var window = GetWindow<BuildWizard>();
+            window.titleContent = WindowTitle;
+            window.Show();
         }
 
-        var projectVersion = Resources.Load<ProjectVersionSO>(ProjectVersion);
-
-        if (projectVersion == null)
+        private void BuildFlow(string version, string[] scenes, string branchName, string folder, BuildOptions options)
         {
-            projectVersion = CreateProjectVersion();
+            var path = GetBuildPath(version, branchName, folder);
+            var buildOptions = GetBuildOptions(options, path, scenes);
+            BuildPlayer(buildOptions);
+            WriteBuildInformation(path, GitExtensions.FullCommitHash, GitExtensions.Branch,
+                _projectVersion.FullVersion);
         }
 
-        return projectVersion;
-    }
+        private void CheckBuildLocation()
+        {
+            if (string.IsNullOrWhiteSpace(buildLocation))
+            {
+                var parentFolder = Path.GetDirectoryName(Application.dataPath);
+                if (string.IsNullOrWhiteSpace(parentFolder))
+                {
+                    return;
+                }
 
-    private string GetFolderBranchName(string branch)
-    {
-        var regex = new Regex(Pattern);
-        var match = regex.Match(branch);
-        return match.Success ? match.Groups[1].Value : string.Empty;
-    }
+                buildLocation = Path.Combine(parentFolder, BuildFolder);
+            }
+        }
 
-    private ProjectVersionSO CreateProjectVersion()
-    {
-        var version = ScriptableObject.CreateInstance<ProjectVersionSO>();
-        AssetDatabase.CreateAsset(version, Path.Combine(Assets, ResourcesFolder, ProjectVersion + AssetFileEnding));
-        AssetDatabase.SaveAssets();
-        return version;
-    }
+        private string[] GetBuildScenes()
+        {
+            var buildScenes = EditorBuildSettings.scenes;
+            var scenes = new string[buildScenes.Length];
+            for (int index = 0; index < buildScenes.Length; index++)
+            {
+                scenes[index] = EditorBuildSettings.scenes[index].path;
+            }
+
+            return scenes;
+        }
+
+        private string SetVersion(bool isRelease = false)
+        {
+            var version = isRelease ? _projectVersion.Version : _projectVersion.FullVersion;
+
+            PlayerSettings.bundleVersion = version;
+
+            return version;
+        }
+
+        private string GetBuildPath(string version, string branch, string folder)
+        {
+            var mainPath = buildLocation;
+
+            EditorPrefs.SetString(EditorBuildLocationKey, buildLocation);
+
+            var buildFolderName = branch + Dash + version;
+            var buildFolderPath = Path.Combine(mainPath, folder, buildFolderName);
+
+            if (!Directory.Exists(buildFolderPath))
+            {
+                Directory.CreateDirectory(buildFolderPath);
+            }
+
+            return Path.Combine(buildFolderPath, FileName);
+        }
+
+        private BuildPlayerOptions GetBuildOptions(BuildOptions option, string path, string[] scenes)
+        {
+            return new BuildPlayerOptions
+            {
+                options = option, locationPathName = path, target = BuildTarget.StandaloneWindows64, scenes = scenes
+            };
+        }
+
+        private void BuildPlayer(BuildPlayerOptions options)
+        {
+            var report = BuildPipeline.BuildPlayer(options);
+            var summary = report.summary;
+
+            if (summary.result is BuildResult.Succeeded)
+            {
+                Debug.Log("Build was a success");
+                return;
+            }
+
+            Debug.LogError("Failed build process!");
+        }
+
+        private void WriteBuildInformation(string path, params string[] data)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            var contents = string.Empty;
+            foreach (var content in data)
+            {
+                contents += content + "\n";
+            }
+
+            var filePath = Path.Combine(Path.GetDirectoryName(path) ?? string.Empty, VersionInformationName);
+            _textFile.WriteToTextFileAt(filePath, contents);
+        }
+
+        private void SetVersionVisuals()
+        {
+            _major = _projectVersion.Major;
+            _minor = _projectVersion.Minor;
+        }
+
+        private ProjectVersionSO FindOrCreateProjectVersion()
+        {
+            var path = Path.Combine(Application.dataPath, ResourcesFolder);
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                CreateProjectVersion();
+            }
+
+            var projectVersion = Resources.Load<ProjectVersionSO>(ProjectVersion);
+
+            if (projectVersion == null)
+            {
+                projectVersion = CreateProjectVersion();
+            }
+
+            return projectVersion;
+        }
+
+        private string GetFolderBranchName(string branch)
+        {
+            var regex = new Regex(Pattern);
+            var match = regex.Match(branch);
+            return match.Success ? match.Groups[2].Value : string.Empty;
+        }
+
+        private ProjectVersionSO CreateProjectVersion()
+        {
+            var version = ScriptableObject.CreateInstance<ProjectVersionSO>();
+            AssetDatabase.CreateAsset(version, Path.Combine(Assets, ResourcesFolder, ProjectVersion + AssetFileEnding));
+            AssetDatabase.SaveAssets();
+            return version;
+        }
     }
 }
