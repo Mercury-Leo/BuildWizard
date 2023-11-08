0ee02d5bac2e9b623e2166069cf33f5c739e600c
Mercury <kansamas@gmail.com>
2023-11-08 18:22:39 +0200

    Update ProjectVersionSO.cs
    - Uses git functions

--- Runtime/ProjectVersion/ProjectVersionSO.cs
+++ Runtime/ProjectVersion/ProjectVersionSO.cs
@@ -1,11 +1,3 @@
-/*
- * This document is the property of Oversight Technologies Ltd that reserves its rights document and to
- * the data / invention / content herein described.This document, including the fact of its existence, is not to be
- * disclosed, in whole or in part, to any other party and it shall not be duplicated, used, or copied in any
- * form, without the express prior written permission of Oversight authorized person. Acceptance of this document
- * will be construed as acceptance of the foregoing conditions.
- */
-
 using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
@@ -24,7 +16,7 @@ namespace ProjectVersion
 
         public string CoreVersion => $"{major}.{minor}";
         public string Version => $"{CoreVersion}.{GitExtensions.GetNumberOfCommits()}";
-        public string FullVersion => $"{Version}-{GitExtensions.CommitHash}";
+        public string FullVersion => $"{Version}-{GitExtensions.GetCommitHash()}";
 
         private void OnValidate()
         {
